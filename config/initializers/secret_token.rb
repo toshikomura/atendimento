06b385339d8fad08e2076d32a9a677f381348245
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-04-10 21:07:40 -0300

    add new project atendimentosb in rails
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atendimentosb::Application.config.secret_token = '070cfa0a8a766fdf03784c029a800dd65a453b68a09c563299b144fd13d7d822be2392db6bf9091183bd5bb3ec2b3c87f6f194d37dd537011cf23be6d72b3b26'
