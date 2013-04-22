21daad2fa9af24a2d6143c453d92c82baded7992
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-04-22 11:18:15 -0300

    Change config of devise to work login and logout
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,10 +4,10 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = "gtk10@inf.ufpr.br"
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = "Devise::Mailer"
+  config.mailer = "Devise::Mailer"
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
@@ -208,7 +208,7 @@ Devise.setup do |config|
   # config.navigational_formats = ["*/*", :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
